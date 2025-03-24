012405227a9fb48233bc163d23ec3823cf755039
Pozdnyakov Bogdan <pozdnyakov.bogdan00@gmail.com>
2025-03-24 14:00:02 +0500

    fix database&cash accessor

--- cache/accessor.py
+++ cache/accessor.py
@@ -1,7 +1,9 @@
 import redis
+from settings import Settings
 
 def get_redis_connection() -> redis.Redis:
-    return redis.Redis(host='localhost', port=6379, db=0)
+    settings = Settings()
+    return redis.Redis(host=settings.CACHE_HOST, port=settings.CACHE_PORT, db=settings.CACHE_DB)
 
 # def set_pomodoro_count():
 #     redis = get_redis_connection()
--- settings.py
+++ settings.py
@@ -5,8 +5,13 @@ class Settings(BaseSettings):
     DB_PORT: int = 5432
     DB_USER: str = 'sa'
     DB_PASSWORD: str = 'pwd'
+    DB_DRIVER: str = 'postgresql+psycopg2:'
     DB_NAME: str = 'pomodoro'
     CACHE_HOST: str = '0.0.0.0'
     CACHE_PORT: int = 6379
     CACHE_DB: int = 0
+
+    @property
+    def db_url(self) -> str:
+        return f'{self.DB_DRIVER}://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self._NAME}'
 
