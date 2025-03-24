b6b8fd8ca5bd3b58c2b7b4d6820e0ee55ff56ad8
Pozdnyakov Bogdan <pozdnyakov.bogdan00@gmail.com>
2025-03-24 13:53:49 +0500

    add db&cash values settings

--- settings.py
+++ settings.py
@@ -1,4 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    sqlite_db_name: str = "pomodoro.db"
+    DB_HOST: str = '0.0.0.0'
+    DB_PORT: int = 5432
+    DB_USER: str = 'sa'
+    DB_PASSWORD: str = 'pwd'
+    DB_NAME: str = 'pomodoro'
+    CACHE_HOST: str = '0.0.0.0'
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
+
